8a4673fdb69ff6bec6b66100145ebcfbc8a51795
Bryan Mytko <bryanmytko@gmail.com>
2022-11-11 23:54:07 -0800

    Add calculate functions

---
+++ lib/calculate.ts
@@ -0,0 +1,21 @@
+const average = (times: number[]): number => {
+  if (!times.length) return 0;
+  return times.reduce((prev, cur) => (prev + cur) / times.length);
+};
+
+const averageOfSize = (times: number[], size: number): number => {
+  if (times.length < size) return 0;
+  return times.slice(-size).reduce((prev, cur) => (prev + cur) / size);
+}
+
+const fastestTime = (times: number[]): number => {
+  if (!times.length) return 0;
+  return Math.min(...times);
+}
+
+const slowestTime = (times: number[]): number => {
+  if (!times.length) return 0;
+  return Math.max(...times);
+}
+
+export { average, averageOfSize, fastestTime, slowestTime };
