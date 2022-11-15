57dd0a21613a9988d09b01aa466c40b7f98939e2
Bryan Mytko <bryanmytko@gmail.com>
2022-11-14 23:27:09 -0800

    Testing inter font

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,16 +1,24 @@
 import { ReactNode } from "react";
 import { Header, Footer } from "../";
+import { Inter } from "@next/font/google";
+
+const inter = Inter({
+  weight: ["400"],
+  variable: "--font-inter",
+});
 
 type LayoutProps = {
-  children: ReactNode
-}
+  children: ReactNode;
+};
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <main className="bg-white dark:bg-gray-800 min-h-screen">
+    <main
+      className={`bg-white dark:bg-gray-800 min-h-screen ${inter.variable}`}
+    >
       <Header />
       {children}
       <Footer />
     </main>
-  )
+  );
 }
