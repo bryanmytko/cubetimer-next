e6e21f8f76ef88c4f6e8e41d691d4c4465405a3c
Bryan Mytko <bryanmytko@gmail.com>
2022-11-19 02:03:52 -0800

    Add CubeDropdown component test

--- __tests__/CubeDropdown.test.jsx
+++ __tests__/CubeDropdown.test.jsx
@@ -1,9 +1,35 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
 import "@testing-library/jest-dom";
 
 import CubeDropdown from "../components/Timer/CubeDropdown";
 
-it("allows the user to change the cube size", () => {
+it("renders the component", () => {
   const { container } = render(<CubeDropdown />);
+
   expect(container).toMatchSnapshot();
+});
+
+it("should correctly set default option", () => {
+  render(<CubeDropdown />);
+
+  expect(screen.getByRole("option", { name: "3x3" }).selected).toBe(true);
+});
+
+it("should display the correct number of options", () => {
+  render(<CubeDropdown />);
+
+  expect(screen.getAllByRole("option").length).toBe(4);
+});
+
+it("should allow user to change cube size", async () => {
+  render(<CubeDropdown dispatch={() => {}} />);
+
+  await userEvent.selectOptions(
+    screen.getByTestId("select"),
+    screen.getByText("4x4")
+  );
+
+  expect(screen.getByRole("option", { name: "4x4" }).selected).toBe(true);
 });
