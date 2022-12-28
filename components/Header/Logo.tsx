6799871ff570f905ad774fc08dd1e34b5f06aa6e
Bryan Mytko <bryanmytko@gmail.com>
2022-12-28 01:32:06 -0800

    Replace <img> with <Image> and remove unused stylesheets

--- components/Header/Logo.tsx
+++ components/Header/Logo.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
+import Image from "next/image";
 import styles from "./Logo.module.css";
 
 const Logo = () => (
   <Link href="/" className="flex items-center">
-    <img className="h-10 p-1" src="/assets/logo.png" />
+    <Image alt="logo" className="h-10 p-1" src="/assets/logo.png" />
     <div className="text-xl">cubetimer.io</div>
   </Link>
 );
