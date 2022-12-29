6efa9b5d7380199dc9300e11885b98aa80c41859
Bryan Mytko <bryanmytko@gmail.com>
2022-12-28 17:08:15 -0800

    Add required Image props

--- components/Header/Logo.tsx
+++ components/Header/Logo.tsx
@@ -4,7 +4,13 @@ import styles from "./Logo.module.css";
 
 const Logo = () => (
   <Link href="/" className="flex items-center">
-    <Image alt="logo" className="h-10 p-1" src="/assets/logo.png" />
+    <Image
+      height="47"
+      width="45"
+      alt="logo"
+      className="h-10 p-1"
+      src="/assets/logo.png"
+    />
     <div className="text-xl">cubetimer.io</div>
   </Link>
 );
