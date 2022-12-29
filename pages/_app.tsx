00c691028ce30fd5180ddd4d89e82ce498099e59
Bryan Mytko <bryanmytko@gmail.com>
2022-12-28 17:45:07 -0800

    Add Head, theme-color

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout } from "../components";
+import Head from "next/head";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <Layout>
+      <Head>
+        <meta name="description" content="An online timer for
+  speedsolving Rubik's cubes and other mechanical puzzles.
+  Sign up and save your past sessions and scrambles!
+  Supports: 2x2, 3x3, 4x4, 5x5, 6x6, 7x7, Pyraminx, Megaminx." />
+        <meta name="theme-color" content="#212121" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
