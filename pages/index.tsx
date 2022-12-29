c0cfb5f29f72ff8a1a58c40c2efcc597dbb3a622
Bryan Mytko <bryanmytko@gmail.com>
2022-12-28 17:07:59 -0800

    Remove gap

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { Timer } from "../components";
 const Home = () => {
   return (
     <div className="container mx-auto">
-      <div className="grid md:grid-cols-6 grid-cols-1 md:gap-4">
+      <div className="grid md:grid-cols-6 grid-cols-1">
         <Timer />
       </div>
     </div>
