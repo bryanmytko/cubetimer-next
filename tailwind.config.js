c0502644cce381c30ba3fbdf8a33e6eb62727b29
Bryan Mytko <bryanmytko@gmail.com>
2023-01-11 05:48:03 -0800

    Define a slightly slower ping animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    extend: {
+      animation: {
+        "ping-slow": "ping 1.1s cubic-bezier(0, 0, 0.2, 1) infinite",
+      },
+    },
     fontFamily: {
       mono: ["Droid Sans", "Monaco", "SFMono-Regular", "ui-monospace"],
     },
