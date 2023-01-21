ef5bb90ff0b49c27d81ed249df1819f08a4e4243
Bryan Mytko <bryanmytko@gmail.com>
2023-01-20 17:37:19 -0800

    Fix missing leading 0 bug

--- lib/format.ts
+++ lib/format.ts
@@ -3,7 +3,17 @@ const humanReadableTime = (t: number, initialValue?: string): string => {
 
   let str = "";
 
-  if (t > 60000) str += `${Math.floor((t / 60000) % 60)}:`;
+  if (t > 60000) {
+    str += `${Math.floor((t / 60000) % 60)}:`;
+    str += `${Math.floor((t / 1000) % 60)
+      .toString()
+      .padStart(2, "0")}:`;
+    str += Math.floor((t / 10) % 100)
+      .toString()
+      .padStart(2, "0");
+    return str;
+  }
+
   str += `${Math.floor((t / 1000) % 60)}:`;
   str += Math.floor((t / 10) % 100)
     .toString()
