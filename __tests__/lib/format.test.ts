a5bd3b478f17bdd52faab292fa192f13c543c963
Bryan Mytko <bryanmytko@gmail.com>
2023-01-20 17:37:36 -0800

    Add tests for leading zero conditions

--- __tests__/lib/format.test.ts
+++ __tests__/lib/format.test.ts
@@ -2,16 +2,28 @@ import { humanReadableTime } from "../../lib/format";
 
 describe("format", () => {
   describe("humanReadableTime", () => {
-    it("formats the time properly for mm:ss", () => {
+    it("formats the time properly for ss:ms", () => {
       const formatted = humanReadableTime(15669);
 
       expect(formatted).toEqual("15:66");
     });
 
-    it("formats the time properly for hh:mm:ss", () => {
+    it("formats the time properly for mm:ss:ms", () => {
       const formatted = humanReadableTime(2315669);
 
       expect(formatted).toEqual("38:35:66");
+    });
+
+    it("adds leading zeroes to seconds when time is over a minute", () => {
+      const formatted = humanReadableTime(1022330);
+
+      expect(formatted).toEqual("17:02:33");
+    });
+
+    it("does not add leading zeroes when time is under a minute", () => {
+      const formatted = humanReadableTime(2330);
+
+      expect(formatted).toEqual("2:33");
     });
   });
 });
