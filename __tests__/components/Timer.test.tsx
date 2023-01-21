166152189da4cf7a99aa7bf0c98d0439a11604cc
Bryan Mytko <bryanmytko@gmail.com>
2023-01-20 17:38:29 -0800

    Mock Scramble.generate() to avoid snapshot mismatch

--- __tests__/components/Timer.test.tsx
+++ __tests__/components/Timer.test.tsx
@@ -1,9 +1,17 @@
-import { render, fireEvent } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
+import Scrambler, { mockGenerate } from "../__mocks__/scrambler";
 import Timer from "../../components/Timer/Timer";
 
+jest.mock("../../lib/scrambler", () => Scrambler);
+
 describe("<Timer>", () => {
+  beforeEach(() => {
+    Scrambler.mockClear();
+    mockGenerate.mockClear();
+  });
+
   it("renders the components", () => {
     const { container } = render(<Timer />);
 
