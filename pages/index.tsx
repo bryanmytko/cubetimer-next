205bfc7c6171a0c69c48dbd3ae106be1686ef946
Bryan Mytko <bryanmytko@gmail.com>
2023-01-23 22:17:44 -0800

    Adjust layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,10 @@ import { Timer } from "../components";
 
 const Home = () => {
   return (
-    <div className="container mx-auto">
-      <div className="grid md:grid-cols-6 grid-cols-1">
-        <Timer />
-      </div>
+    <div className="grid md:grid-cols-6 grid-cols-1">
+      <Timer />
     </div>
   );
 };
+
 export default Home;
