a5c5bceb8ef5565b6479ca39b0b8c61415ca3107
Bryan Mytko <bryanmytko@gmail.com>
2023-02-23 11:30:30 -0800

    Formatting

--- components/Timer/Times.tsx
+++ components/Timer/Times.tsx
@@ -16,17 +16,16 @@ type TimerAction = { type: TimerActionKind.REMOVE_TIME; index: number };
 const Times = (props: TimesProps) => {
   const { dispatch, solveTimes } = props;
   return (
-    <div className="mt-6 md:mt-12 w-11/12 mx-auto">
-      <h3 className="text-md text-gray-100 mx-auto text-center mb-2">Times</h3>
-      <div className="bg-neutral-800 rounded pt-2 pb-4 overflow-auto snap-y snap-mandatory">
-        <ul className="h-96 text-gray-300 mx-auto w-11/12 pr-4">
+    <div className="w-11/12 mx-auto mt-6 md:mt-12">
+      <h3 className="mx-auto mb-2 text-center text-gray-100 text-md">Times</h3>
+      <div className="pt-2 pb-4 overflow-auto rounded bg-neutral-800 snap-y snap-mandatory">
+        <ul className="w-11/12 pr-4 mx-auto text-gray-300 h-96">
           {solveTimes.map((time: number, index: number) => (
             <li
               key={index}
               id={String(index)}
-              className={`px-2 py-1 cursor-pointer last:snap-end ${
-                index % 2 == 0 ? "bg-neutral-800" : "bg-neutral-700"
-              }`}
+              className={`px-2 py-1 cursor-pointer last:snap-end ${index % 2 == 0 ? "bg-neutral-800" : "bg-neutral-700"
+                }`}
               onClick={() =>
                 dispatch({ type: TimerActionKind.REMOVE_TIME, index })
               }
