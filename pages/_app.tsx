99f12916373e3becfea8416e0a6d3f4e5f8cc7f6
Bryan Mytko <bryanmytko@gmail.com>
2023-02-26 22:07:48 -0800

    Add AppType

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppProps, AppType } from "next/app";
 import Head from "next/head";
 import { SessionProvider } from "next-auth/react"
 
 import { Layout } from "../components";
 
-const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
+const App: AppType = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
   return (
     <>
       <Head>
