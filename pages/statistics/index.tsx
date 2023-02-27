03f1a4b732ab83f7ae3311122ab288eeac1beb07
Bryan Mytko <bryanmytko@gmail.com>
2023-02-27 12:55:15 -0800

    Add basic gql query for solves

--- pages/statistics/index.tsx
+++ pages/statistics/index.tsx
@@ -1,54 +1,31 @@
-import React from 'react';
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+
+import type { Solve } from "@prisma/client";
+
+const AllSolvesQuery = gql`
+  query {
+    solves {
+      scramble
+      puzzle
+      time
+    }
+  }
+`;
 
 const Statistics = () => {
-  return <div className="container statistics-container">
-    <h1>Statistics</h1>
-    <div className="row">
-      <div className="col s12">
-        <p>Best Time: 19.34</p>
-        <p>Best Average: 22.21</p>
-        <table>
-          <tr>
-            <th>Date</th>
-            <th>Average</th>
-            <th colSpan={13}>Times</th>
-          </tr>
-          <tr>
-            <td>11/5/1955</td>
-            <td>19.87</td>
-            <td>14.44</td>
-            <td>21.37</td>
-            <td>20.43</td>
-            <td>19.87</td>
-            <td>19.21</td>
-            <td>21.33</td>
-            <td>22.33</td>
-            <td>13.87</td>
-            <td>11.89</td>
-            <td>14.97</td>
-            <td>18.02</td>
-            <td>13.33</td>
-          </tr>
-          <tr>
-            <td>11/3/1955</td>
-            <td>19.87</td>
-            <td>14.44</td>
-            <td>21.37</td>
-            <td>20.43</td>
-            <td>19.87</td>
-            <td>19.21</td>
-            <td>21.33</td>
-            <td>22.33</td>
-            <td>13.87</td>
-            <td>11.89</td>
-            <td>14.97</td>
-            <td>18.02</td>
-            <td>13.33</td>
-          </tr>
-        </table>
-      </div>
+  const { data, loading, error } = useQuery(AllSolvesQuery);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Oh no... {error.message}</p>;
+
+  console.log("Solve data:", data);
+
+  return (
+    <div className="text-white container statistics-container">
+      <h1>Statistics</h1>
     </div>
-  </div>;
+  );
 };
 
 export default Statistics;
