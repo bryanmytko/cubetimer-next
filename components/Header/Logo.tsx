3e263b03473b32204b617c62a6b08e9cec3b960b
Bryan Mytko <bryanmytko@gmail.com>
2023-03-01 19:27:37 -0800

    Add dark prop for logo

--- components/Header/Logo.tsx
+++ components/Header/Logo.tsx
@@ -1,17 +1,27 @@
 import Link from "next/link";
 import Image from "next/image";
 
-const Logo = () => (
-  <Link href="/" className="flex items-center">
-    <Image
-      height="47"
-      width="45"
-      alt="logo"
-      className="h-10 p-1"
-      src="/assets/logo.png"
-    />
-    <div className="text-xl text-white">cubetimer.io</div>
-  </Link>
-);
+interface LogoProps {
+  dark?: boolean;
+}
+
+const Logo = (props: LogoProps) => {
+  const { dark } = props;
+
+  return (
+    <Link href="/" className="flex items-center">
+      <Image
+        height="47"
+        width="45"
+        alt="logo"
+        className="h-10 p-1"
+        src="/assets/logo.png"
+      />
+      <div className={`text-xl ${dark ? "text-black" : "text-white"}`}>
+        cubetimer.io
+      </div>
+    </Link>
+  );
+};
 
 export default Logo;
--- components/Header/Nav.tsx
+++ components/Header/Nav.tsx
@@ -9,8 +9,6 @@ const Nav = () => {
   const { data: session } = useSession();
   const auth = () => {
     if (session) {
-      // @TODO google avatars sometimes 403
-      // const avatarUrl = session.user?.image || gravatar.url(session.user?.email || "", { protocol: 'https', s: '100' });
       const avatarUrl = gravatar.url(session.user?.email || "", {
         protocol: "https",
         s: "100",
@@ -103,50 +101,16 @@ const Nav = () => {
   };
 
   return (
-    <nav className="px-2 sm:px-4">
+    <nav className="px-2">
       <div className="container flex flex-wrap items-center justify-between mx-auto">
         <Logo />
-        {/* <button
-          data-collapse-toggle="navbar-default"
-          type="button"
-          className="inline-flex items-center p-2 ml-3 text-sm bg-gray-700 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:focus:ring-gray-600"
-          aria-controls="navbar-default"
-          aria-expanded="false"
-        >
-          <span className="sr-only">Open Navigation</span>
-          <svg
-            className="w-6 h-6"
-            aria-hidden="true"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 
-              011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 
-              110 2H4a1 1 0 01-1-1z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-        </button> */}
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="flex flex-col p-4 mt-4 border md:flex-row md:space-x-6 md:mt-0 md:text-sm md:font-medium md:border-0">
+        <div className="block w-auto" id="navbar-default">
+          <ul className="flex p-4 flex-row space-x-6 text-sm font-medium border-0">
             <li>
               <Link className="block py-2 text-white" href="/">
                 Timer
               </Link>
             </li>
-            {/* <li>
-              <Link className="block py-2 text-white" href="statistics">
-                Statistics
-              </Link>
-            </li> */}
-            {/* <li>
-              <Link className="block py-2 text-white" href="profile">
-                Profile
-              </Link>
-            </li> */}
             <li>
               <Link className="block py-2 text-white" href="/tutorials">
                 Tutorials
