150dc1d92640d7a4d8cb98db975fcee403bd1ab3
Bryan Mytko <bryanmytko@gmail.com>
2023-03-02 12:56:52 -0800

    Add session

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import { useSession } from "next-auth/react";
 
 const Profile = () => {
+  const { data: session } = useSession();
+  const displayName = session ? session.user.name : "User";
+
   return <div className="container profile-container">
-    <h1>Profile</h1>
+    <h1 className="text-3xl text-white">{`${displayName}'s`} Profile</h1>
   </div>;
 };
 
