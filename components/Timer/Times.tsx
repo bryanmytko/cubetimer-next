ff095a27e5358b3405bfcba22a81acbf0d34f034
Bryan Mytko <bryanmytko@gmail.com>
2023-03-03 03:22:25 -0800

    add deleteTime call to handle full deletion if required

--- components/Timer/Times.tsx
+++ components/Timer/Times.tsx
@@ -22,7 +22,9 @@ const Times = (props: TimesProps) => {
 
   const deleteTime = (index: number, solveId: number | string) => {
     dispatch({ type: TimerActionKind.REMOVE_TIME, index });
-    deleteSolve({ variables: { id: solveId } })
+    // @TODO this doesn't work right now
+    // Check: id type, currently string | number, need to pick one
+    //deleteSolve({ variables: { id: solveId } })
   };
 
   return (
