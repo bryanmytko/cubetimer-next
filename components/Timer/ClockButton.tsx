f63fbf66910f52b15d0ae17f8a324f13a3994abb
Bryan Mytko <bryanmytko@gmail.com>
2023-03-03 16:14:55 -0800

    Refactor to use shared handler

--- components/Timer/ClockButton.tsx
+++ components/Timer/ClockButton.tsx
@@ -1,26 +1,11 @@
-import { Dispatch } from "react";
-import { TimerAction } from "../../types/timer";
-import { TimerActionKind } from "../../reducers";
-
 interface ClockButtonProps {
-  dispatch: Dispatch<TimerAction>;
-  inspectionRunning: boolean;
-  inspectionTime: number;
+  handleKeyup: (e: KeyboardEvent | React.MouseEvent) => Promise<void>;
   ready: boolean;
-  running: boolean;
   sessionComplete: boolean;
 }
 
 const ClockButton = (props: ClockButtonProps) => {
-  const { dispatch, inspectionTime, inspectionRunning, ready, running, sessionComplete } = props;
-
-  const toggleTimer = () => {
-    if (inspectionRunning) return;
-
-    inspectionTime && !running
-      ? dispatch({ type: TimerActionKind.TOGGLE_INSPECTION })
-      : dispatch({ type: TimerActionKind.TOGGLE_RUNNING });
-  };
+  const { handleKeyup, ready, sessionComplete } = props;
 
   const buttonColor = () => {
     if (sessionComplete) return "bg-green-500";
@@ -28,14 +13,15 @@ const ClockButton = (props: ClockButtonProps) => {
     return "bg-yellow-300";
   };
 
-  const buttonText = () => sessionComplete ? "Session complete!" : "Press spacebar or click to start!";
+  const buttonText = () =>
+    sessionComplete ? "Session complete!" : "Press spacebar or click to start!";
 
   return (
     <button
       id="timer-btn"
       className={`timer-btn-start block mx-auto mt-6 px-10 py-5 text-3xl 
         rounded-md w-11/12 ${buttonColor()}`}
-      onClick={toggleTimer}
+      onClick={handleKeyup}
     >
       {buttonText()}
     </button>
