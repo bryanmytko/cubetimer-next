2fe4b644c022360ab2080d4f8fb5207c4390182e
Bryan Mytko <bryanmytko@gmail.com>
2023-03-06 17:52:09 -0800

    Spit some mock data out on the profile page

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,11 +1,22 @@
 import { useSession } from "next-auth/react";
+import Image from "next/image";
+import gravatar from "gravatar";
 
 const Profile = () => {
   const { data: session } = useSession();
   const displayName = session ? session.user.name : "User";
+  const photoUrl = gravatar.url(session ? session.user?.email : "", {
+    protocol: "https",
+    s: "100",
+  });
 
   return <div className="container profile-container">
     <h1 className="text-3xl text-white">{`${displayName}'s`} Profile</h1>
+    <Image height={100} width={100} alt="profile pic" src={photoUrl} />
+    <div className="text-white rounded-md card border-1">
+      <p>Fastest time: 12.32</p>
+      <p>Best Average: 16.83</p>
+    </div>
   </div>;
 };
 
