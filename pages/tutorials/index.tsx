1486ab99f16e521e78ef3df6e593851f0b20cb2c
Bryan Mytko <bryanmytko@gmail.com>
2023-03-15 13:56:43 -0400

    Add basic template for tutorials page

--- pages/tutorials/index.tsx
+++ pages/tutorials/index.tsx
@@ -1,36 +1,27 @@
 const Tutorials = () => {
   return (
-    <div className="text-white">
-      <h1 className="text-3xl">Tutorials</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus libero
-        lacus, euismod vulputate ornare sit amet, lobortis sed orci. Sed sit
-        amet euismod mi, eget vestibulum magna. Ut sed bibendum risus, id dictum
-        sem. Etiam at scelerisque enim. Aenean tristique ac nulla quis suscipit.
-        Duis et arcu in ante aliquam gravida. Praesent semper ullamcorper lorem
-        quis ullamcorper. Cras auctor nisl sed lorem convallis, nec feugiat arcu
-        feugiat. Vestibulum nisl nibh, ullamcorper at magna at, aliquet interdum
-        nulla. Maecenas malesuada risus non massa sodales elementum. Phasellus
-        tincidunt quam eget turpis blandit, et mattis justo varius. Aenean
-        ultricies laoreet consectetur. Vestibulum egestas dolor in felis posuere
-        imperdiet. Integer iaculis mauris aliquet dolor mollis porta. Etiam
-        efficitur faucibus massa, in tincidunt felis fringilla eleifend. Donec
-        posuere dapibus tortor a ullamcorper.{" "}
-      </p>
-      <p>
-        Ut mi mi, tincidunt vel malesuada ut, varius et nisi. Vestibulum eu
-        metus consequat, bibendum ante et, ultricies dui. Donec quis elit
-        ligula. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Sed libero lectus, pretium nec mauris
-        et, maximus condimentum libero. Phasellus sed vestibulum elit. Aenean
-        nec nisi in quam egestas porta sed quis mi. Nam vitae arcu ac nibh
-        dignissim volutpat maximus et tellus. Aenean facilisis nunc et velit
-        fringilla, nec hendrerit nulla finibus. Sed id mollis magna, id semper
-        odio. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris sollicitudin lobortis augue, sed
-        lacinia nulla viverra et. Sed et sollicitudin nisl, at bibendum nulla.
-        Sed a urna eleifend, ultricies velit vel, posuere lectus.
-      </p>
+    <div className="bg-gray-200 m-auto p-12 mt-12 w-11/12 rounded text-black">
+      <h1 className="text-3xl font-bold m-auto mb-6">Tutorials</h1>
+      <ul>
+        <li>
+          <a className="text-cyan-800" href="http://badmephisto.com">
+            Bad Mephisto
+          </a>
+        </li>
+        <li>
+          <a className="text-cyan-800" href="https://jperm.net">
+            JPerm
+          </a>
+        </li>
+        <li>
+          <a
+            className="text-cyan-800"
+            href="http://cubefreak.net/speed/articles/howtolearn.php"
+          >
+            Cubefreak
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
