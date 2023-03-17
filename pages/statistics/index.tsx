6407d2d6c99b82460620a909a2a4755568a016b6
Bryan Mytko <bryanmytko@gmail.com>
2023-03-17 13:57:23 -0400

    Add tables for stats; rough styling

--- pages/statistics/index.tsx
+++ pages/statistics/index.tsx
@@ -2,7 +2,10 @@ import { useQuery } from "@apollo/client";
 
 import { humanReadableTime } from "../../lib/format";
 import type { Solve } from "@prisma/client";
-import { SOLVES_FOR_USER, SOLVE_SESSIONS_FOR_USER } from "../../graphql/queries";
+import {
+  SOLVES_FOR_USER,
+  SOLVE_SESSIONS_FOR_USER,
+} from "../../graphql/queries";
 import { useSession } from "next-auth/react";
 
 interface SolveSession {
@@ -20,33 +23,58 @@ const Statistics = () => {
   const { data: solveSessionsData } = useQuery(SOLVE_SESSIONS_FOR_USER, {
     skip: !session,
     variables: { userId },
-  })
-
-  const displaySolve = (solve: Solve) => {
-    return (<p key={solve.id}>
-      {humanReadableTime(parseInt(solve.time))
-      }: {" "}
-      <code className="inline">{solve.scramble}</code>
-    </p >);
-  }
+  });
 
   return (
-    <div className="container text-white statistics-container">
-      <h1 className="mt-6 text-3xl">Statistics</h1>
+    <div className="container statistics-container bg-gray-200 m-auto p-12 mt-12 w-11/12 rounded text-black">
+      <h1 className="text-3xl">Past Solves</h1>
       <div className="p-6 mt-4 border">
-        {solveData &&
-          solveData.solves.map(displaySolve)}
+        <table className="bg-slate-500">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Time
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Scramble
+              </th>
+            </tr>
+          </thead>
+          {solveData &&
+            solveData.solves.map((solve: Solve, index: number) => {
+              return (
+                <tr
+                  key={index}
+                  className={`px-6 py-3 ${
+                    index % 2 === 0 ? "bg-slate-400" : "bg-slate-800"
+                  }`}
+                >
+                  <td>{humanReadableTime(parseInt(solve.time))}: </td>
+                  <td>{solve.scramble}</td>
+                </tr>
+              );
+            })}
+        </table>
       </div>
       <h2 className="mt-6 text-3xl">Classic Mode Sessions</h2>
       {solveSessionsData &&
         solveSessionsData.solveSessionsForUser.map((session: SolveSession) => {
-          return <div key={session.id} className="p-6 mt-4 border">
-            <h3 className="text-xl">Session ${session.id}</h3>
-            {session.solves.map(displaySolve)}
-          </div>;
-        })
-      }
-    </div>);
+          return (
+            <div key={session.id} className="p-6 mt-4 border">
+              <h3 className="text-xl">Session ${session.id}</h3>
+              {session.solves.map((solve: Solve, index: number) => {
+                return (
+                  <tr key={index}>
+                    <td>{humanReadableTime(parseInt(solve.time))}: </td>
+                    <td>{solve.scramble}</td>
+                  </tr>
+                );
+              })}
+            </div>
+          );
+        })}
+    </div>
+  );
 };
 
 export default Statistics;
