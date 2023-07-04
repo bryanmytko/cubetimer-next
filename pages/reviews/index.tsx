2f1f686347363fe39017bbc22d13f30d67b05ba3
Bryan Mytko <bryanmytko@gmail.com>
2023-07-03 22:22:29 -0400

    Add front end design for review page

--- pages/reviews/index.tsx
+++ pages/reviews/index.tsx
@@ -1,9 +1,136 @@
+import Image from "next/image";
+
 const Reviews = () => {
   return (
     <div className="container profile-container">
-      <div className="bg-gray-200 m-auto p-12 mt-12 w-11/12 rounded text-black">
-        <h1 className="text-3xl font-bold m-auto mb-6">Reviews</h1>
-        <p>No reviews yet!</p>
+      <div className="bg-gray-200 m-auto p-12 w-11/12 mt-4 text-black">
+        <div className="grid grid-cols-3 gap-20">
+          <div className="col-span-2">
+            <h1 className="text-3xl font-bold m-auto mb-6">
+              Top 5 Speed Cubes for 2023 (What Rubik's Cube to buy)
+            </h1>
+            <div>
+              <Image
+                src="https://en.gravatar.com/userimage/4046405/3db0ec823880c20cf6c1ab24a9ad4843"
+                alt="author"
+                height={30}
+                width={30}
+                className="inline-block"
+              />
+              <span>
+                <a href="#" className="px-2 text-blue-700">
+                  Bryan Mytko
+                </a>
+                <h2 className="inline-block m-auto">July 3rd, 2023</h2>
+              </span>
+            </div>
+            <Image
+              src="https://i.imgur.com/wLJlOOY.jpeg"
+              height={900}
+              width={900}
+              alt="preview"
+              className="my-6"
+            />
+            <p className="mb-6">
+              <h3 className="text-2xl font-bold mb-6">
+                1. GAN 365 M, 3x3 Magnetic Speed Cube (
+                <a
+                  className="text-blue-700"
+                  href="https://www.amazon.com/GAN-Magnetic-Speed-Stickerless-Magic/dp/B086PNKX2P/ref=sr_1_5"
+                >
+                  buy on Amazon
+                </a>
+                )
+              </h3>
+              <a
+                className="text-blue-700"
+                href="https://www.amazon.com/GAN-Magnetic-Speed-Stickerless-Magic/dp/B086PNKX2P/ref=sr_1_5"
+              >
+                GAN 356 M, 3x3 Magnetic Speed Cube Stickerless Gans
+              </a>{" "}
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+              vel sodales libero. Phasellus luctus laoreet rutrum. Aliquam in
+              purus sed leo interdum vehicula non nec felis. Vivamus vitae neque
+              lorem. Maecenas ut ante ut urna interdum varius vitae eu mauris.
+              Proin euismod arcu sit amet aliquam ultricies. Nulla mi urna,
+              accumsan nec tempor eu, rhoncus quis justo. Cras non imperdiet
+              sapien, ac pharetra ligula. Fusce placerat tortor sit amet orci
+              cursus, vitae convallis augue congue. In tincidunt, lorem eu
+              vulputate tincidunt, nulla quam sagittis nisi, et sodales diam
+              diam vitae leo. Suspendisse pharetra tortor sed vehicula placerat.
+              Suspendisse condimentum justo a nisl dignissim ornare. Nulla a ex
+              magna. Mauris non aliquam lectus, non pharetra magna. Nunc vel
+              turpis sit amet mauris iaculis sollicitudin et a lectus.
+              Pellentesque vestibulum libero nulla, ac iaculis velit pharetra
+              auctor.
+            </p>
+
+            <p className="mb-6">
+              Sed nec velit lectus. Nunc varius odio vitae nisi vehicula, vitae
+              euismod sem pellentesque. Integer rutrum sollicitudin ullamcorper.
+              Proin vestibulum placerat finibus. Aliquam viverra mi nec ligula
+              porttitor, non volutpat erat aliquet. Donec eu fermentum eros.
+              Etiam scelerisque diam eu commodo dignissim. Mauris vulputate, sem
+              sit amet imperdiet imperdiet, dui dui suscipit ex, eu imperdiet
+              libero eros in lacus. In vestibulum egestas mi in vulputate. Fusce
+              eu mollis orci. Nulla in venenatis tellus. Nam nec sollicitudin
+              lorem. Maecenas quam purus, hendrerit at nibh quis, malesuada
+              commodo elit.
+            </p>
+
+            <p className="mb-6">
+              Nullam auctor porttitor metus sit amet lacinia. Etiam convallis ac
+              tellus a posuere. Vivamus in diam auctor, tempus turpis eget,
+              eleifend purus. Curabitur id dapibus augue, eu malesuada purus.
+              Phasellus sagittis consequat ullamcorper. Curabitur id nibh a erat
+              laoreet luctus. Nam elementum nisi sed nisl vulputate feugiat.
+              Aenean malesuada suscipit ipsum ac imperdiet. Maecenas facilisis
+              auctor lectus nec consectetur.
+            </p>
+
+            <p className="mb-6">
+              In venenatis scelerisque tellus. Morbi sollicitudin faucibus
+              dolor, sit amet ultricies lorem vestibulum eget. Pellentesque nec
+              ullamcorper leo. In in erat rhoncus, vestibulum massa sed, porta
+              nulla. Nullam pretium nisl non arcu scelerisque, non dignissim
+              eros laoreet. Morbi justo nunc, vehicula et sapien vestibulum,
+              sagittis tincidunt urna. Aliquam et leo ut felis vulputate
+              pretium.
+            </p>
+          </div>
+          <div>
+            <h2 className="mt-20 pt-6 text-cyan-800 font-bold text-xl">
+              Rubik's Cube Tutorials
+            </h2>
+            <p className="my-4 border-b-gray-300 border-2 pb-2 mr-20">
+              Beginner's Solution
+            </p>
+            <p className="my-4 border-b-gray-300 border-2 pb-2 mr-20">
+              Advanced Solution
+            </p>
+            <p className="my-4 border-b-gray-300 border-2 pb-2 mr-20">
+              Expert Solution
+            </p>
+            <h2 className="mt-10 pt-6 text-cyan-800 font-bold text-xl">
+              Other Reviews
+            </h2>
+            <p className="my-4 border-b-gray-300 border-2 pb-2 mr-20">
+              GAN 4x4
+            </p>
+            <p className="my-4 border-b-gray-300 border-2 pb-2 mr-20">
+              QiYi Warrior S 3x3 Stickerless
+            </p>
+            <p className="my-4 border-b-gray-300 border-2 pb-2 mr-20">
+              AA Foo Bar 3x3 Stickerless
+            </p>
+            <p className="my-4 border-b-gray-300 border-2 pb-2 mr-20">
+              GAN 5x5 Tiled Stickerless
+            </p>
+            <p className="my-4 border-b-gray-300 border-2 pb-2 mr-20">
+              MoYu WeiLong GTS 3M
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
