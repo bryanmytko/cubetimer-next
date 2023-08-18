9b0d62174e04fb021b7dda52d59dead3be24e620
Bryan Mytko <bryanmytko@gmail.com>
2023-08-18 13:45:45 -0400

    Change styling

--- src/components/Statistics/Solves.tsx
+++ src/components/Statistics/Solves.tsx
@@ -13,7 +13,7 @@ const Solves = (props: SolvesProps) => {
 
   return (
     <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-      <thead className="text-xs text-black uppercase bg-green-600">
+      <thead className="text-xs text-black uppercase bg-yellow-400">
         <tr>
           <th scope="col" className="pl-6 pr-1 py-3">
             Time
