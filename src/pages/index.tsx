f50fe26449c873ff2cbc39032756568373074ce0
Bryan Mytko <bryanmytko@gmail.com>
2023-08-19 11:07:03 -0400

    Add GA

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
+import { GoogleAnalytics } from "nextjs-google-analytics";
+
 import { Timer } from "../components";
 
 const Home = () => {
   return (
     <div className="grid md:grid-cols-6 grid-cols-1">
+      <GoogleAnalytics trackPageViews />
       <Timer />
     </div>
   );
