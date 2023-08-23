a70eb3e29b68856042444d0fbe9b281623dd639b
Bryan Mytko <bryanmytko@gmail.com>
2023-08-22 20:59:07 -0400

    Expose HumanReadableTime

--- src/components/Timer/index.ts
+++ src/components/Timer/index.ts
@@ -1,6 +1,7 @@
 export { default as ClassicModeTimes } from "./ClassicModeTimes";
 export { default as Clock } from "./Clock";
 export { default as ClockButton } from "./ClockButton";
+export { default as HumanReadableTime } from "./HumanReadableTime";
 export { default as Panel } from "./Panel";
 export { default as Timer } from "./Timer";
 export { default as Times } from "./Times";
