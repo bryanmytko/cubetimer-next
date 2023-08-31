8aa70cbd3be3309b42b6ba9dd211cf26bb82d656
Bryan Mytko <bryanmytko@gmail.com>
2023-08-31 01:57:59 -0400

    Implement forward pagination; migrate Solve data to its own component to avoid multple fetch queries

--- src/components/Statistics/index.tsx
+++ src/components/Statistics/index.tsx
@@ -1,2 +1,3 @@
-export { default as Solves } from "./Solves";
 export { default as ClassicSolves } from "./ClassicSolves";
+export { default as Error } from "./Error";
+export { default as Solves } from "./Solves";
