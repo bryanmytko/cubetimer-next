c7889017d3b5e639c6bcbd2c9d4e991f30763657
Bryan Mytko <bryanmytko@gmail.com>
2023-08-31 01:59:01 -0400

    Add pagination fields to query

--- src/graphql/queries/solveSessionsForUser.ts
+++ src/graphql/queries/solveSessionsForUser.ts
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const SOLVE_SESSIONS_FOR_USER = gql`
-  query SolveSessionsForUser($userId: String!){
+  query SolveSessionsForUser($userId: String!) {
     solveSessionsForUser(userId: $userId) {
       id
       solves {
--- src/graphql/queries/solvesForUser.ts
+++ src/graphql/queries/solvesForUser.ts
@@ -1,12 +1,23 @@
 import { gql } from "@apollo/client";
 
 export const SOLVES_FOR_USER = gql`
-  query SolvesForUser($userId: String!){
-    solves(userId: $userId) {
-      id
-      puzzle
-      scramble
-      time
+  query SolvesForUser($first: Int, $after: ID, $userId: String!) {
+    solves(first: $first, after: $after, userId: $userId) {
+      pageInfo {
+        startCursor
+        endCursor
+        hasPreviousPage
+        hasNextPage
+      }
+      edges {
+        cursor
+        node {
+          id
+          puzzle
+          scramble
+          time
+        }
+      }
     }
   }
 `;
