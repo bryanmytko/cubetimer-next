defc33ce7a58a19e4c1ddcd99cc60710057f0e16
Bryan Mytko <bryanmytko@gmail.com>
2023-09-04 00:57:39 -0400

    Fix syntax error

--- src/migrations/20230904041630_created_at_not_null/migration.sql
+++ src/migrations/20230904041630_created_at_not_null/migration.sql
@@ -1,17 +1,17 @@
 -- AlterTable
-ALTER TABLE "Account" ALTER COLUMN "createdAt" SET NOT NULL
+ALTER TABLE "Account" ALTER COLUMN "createdAt" SET NOT NULL;
 
 -- AlterTable
-ALTER TABLE "Session" ALTER COLUMN "createdAt" SET NOT NULL
+ALTER TABLE "Session" ALTER COLUMN "createdAt" SET NOT NULL;
 
 -- AlterTable
-ALTER TABLE "Solve" ALTER COLUMN "createdAt" SET NOT NULL
+ALTER TABLE "Solve" ALTER COLUMN "createdAt" SET NOT NULL;
 
 -- AlterTable
-ALTER TABLE "SolveSession" ALTER COLUMN "createdAt" SET NOT NULL
+ALTER TABLE "SolveSession" ALTER COLUMN "createdAt" SET NOT NULL;
 
 -- AlterTable
-ALTER TABLE "User" ALTER COLUMN "createdAt" SET NOT NULL
+ALTER TABLE "User" ALTER COLUMN "createdAt" SET NOT NULL;
 
 -- AlterTable
-ALTER TABLE "VerificationToken" ALTER COLUMN "createdAt" SET NOT NULL
+ALTER TABLE "VerificationToken" ALTER COLUMN "createdAt" SET NOT NULL;
