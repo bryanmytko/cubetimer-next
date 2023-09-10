14ef20db0aa5bdb59267bf7c2dafb7dec303e0a3
Bryan Mytko <bryanmytko@gmail.com>
2023-09-10 18:54:47 -0400

    Add initial modal value

--- src/components/Timer/initialState.ts
+++ src/components/Timer/initialState.ts
@@ -3,6 +3,7 @@ import { TimerState } from "../../types/timer";
 export const initialState: TimerState = {
   classicModeEnabled: false,
   classicModeLength: 12,
+  confirmActive: false,
   countdown: 0,
   inspectionRunning: false,
   inspectionTime: 0,
