0fd892e59ba4cfc7bfce1baa9424da25f5cb1857
Bryan Mytko <bryanmytko@gmail.com>
2023-09-11 00:38:53 -0400

    Update the dropdown to use puzzle types instead of strings

--- src/components/Timer/CubeDropdown.tsx
+++ src/components/Timer/CubeDropdown.tsx
@@ -1,9 +1,13 @@
 import { Dispatch, SyntheticEvent } from "react";
-import { TimerActionKind } from "../../types/timer";
+import {
+  PuzzleNameObj,
+  PuzzleValueType,
+  TimerActionKind,
+} from "../../types/timer";
 
 type TimerAction = {
   type: TimerActionKind.PUZZLE_TYPE;
-  puzzle: string;
+  puzzle: PuzzleValueType;
 };
 
 interface CubeDropdownProps {
@@ -14,7 +18,10 @@ interface CubeDropdownProps {
 const CubeDropdown = (props: CubeDropdownProps) => {
   const { dispatch, inspectionRunning } = props;
   const onChange = (e: SyntheticEvent & { target: HTMLSelectElement }) => {
-    dispatch({ type: TimerActionKind.PUZZLE_TYPE, puzzle: e.target?.value });
+    dispatch({
+      type: TimerActionKind.PUZZLE_TYPE,
+      puzzle: e.target?.value,
+    });
   };
 
   return (
@@ -27,10 +34,10 @@ const CubeDropdown = (props: CubeDropdownProps) => {
         data-testid="select"
         disabled={inspectionRunning}
       >
-        <option value="2x2">2x2</option>
-        <option value="3x3">3x3</option>
-        <option value="4x4">4x4</option>
-        <option value="5x5">5x5</option>
+        <option value={PuzzleNameObj["2x2"]}>2x2</option>
+        <option value={PuzzleNameObj["3x3"]}>3x3</option>
+        <option value={PuzzleNameObj["4x4"]}>4x4</option>
+        <option value={PuzzleNameObj["5x5"]}>5x5</option>
       </select>
     </>
   );
