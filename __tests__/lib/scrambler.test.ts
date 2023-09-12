58784ca7dd835bb762b5f69893e28ebd2a534b88
Bryan Mytko <bryanmytko@gmail.com>
2023-09-11 20:55:03 -0400

    Use PuzzleNameObj types, update refs

--- __tests__/lib/scrambler.test.ts
+++ __tests__/lib/scrambler.test.ts
@@ -1,16 +1,17 @@
-import Scrambler from "../../lib/scrambler";
+import Scrambler from "../../src/lib/scrambler";
+import { PuzzleNameObj } from "../../src/types/timer";
 
 describe("Scrambler", () => {
   it("generates a scramble", () => {
-    const scrambler = new Scrambler("3x3");
+    const scrambler = new Scrambler();
     const scramble = scrambler.generate();
 
     expect(scramble).toMatch(/^[UDRLBF'2\s]*$/);
   });
 
   it("generates a scramble of the appropriate size", () => {
-    const scrambler = new Scrambler("3x3");
-    const scramble = scrambler.generate();
+    const scrambler = new Scrambler();
+    const scramble = scrambler.generate(PuzzleNameObj["3x3"]);
 
     expect(scramble.split(" ")).toHaveLength(21);
   });
