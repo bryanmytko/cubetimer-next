5d1c83cb0ebb25c0cf35da6bb0d19522eaf1fac6
Bryan Mytko <bryanmytko@gmail.com>
2023-10-12 03:29:24 -0400

    Testing providers vercel

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -4,6 +4,8 @@ import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "../../../lib/prismadb";
 
+console.log("secret", process.env.NEXTAUTH_SECRET);
+
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
   callbacks: {
@@ -17,13 +19,13 @@ export default NextAuth({
   },
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID || "",
-      clientSecret: process.env.GITHUB_SECRET || "",
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-    }),
+    // GitHubProvider({
+    //   clientId: process.env.GITHUB_ID || "",
+    //   clientSecret: process.env.GITHUB_SECRET || "",
+    // }),
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_CLIENT_ID || "",
+    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    // }),
   ],
 });
