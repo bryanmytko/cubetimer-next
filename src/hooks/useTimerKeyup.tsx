e135c2341096685c7eacf52317d8acced867ad64
Bryan Mytko <bryanmytko@gmail.com>
2023-10-14 20:01:55 -0400

    add useTimerKeyup

---
+++ src/hooks/useTimerKeyup.tsx
@@ -0,0 +1,44 @@
+import { useCallback, Dispatch } from "react";
+
+import { TimerActionKind, TimerAction } from "../types/timer";
+
+const TIMEOUT = 60;
+
+const useTimerKeyup = (
+  keyLocked: boolean,
+  dispatch: Dispatch<TimerAction>,
+  keyMaps: any
+) => {
+  return useCallback(
+    (e: KeyboardEvent | React.MouseEvent) => {
+      if (keyLocked) return; // Avoid race condition with mouse + keyboard
+      dispatch({ type: TimerActionKind.SET_KEY_LOCKED, value: true });
+      setTimeout(
+        () => dispatch({ type: TimerActionKind.SET_KEY_LOCKED, value: false }),
+        TIMEOUT
+      );
+
+      if (e.type === "click") return keyMaps[" "];
+
+      switch ((e as KeyboardEvent).key) {
+        case " ":
+          e.preventDefault();
+          keyMaps[" "]();
+          break;
+        case "Escape":
+          e.preventDefault();
+          keyMaps["Escape"]();
+          break;
+        case "Enter":
+          e.preventDefault();
+          keyMaps["Enter"]();
+          break;
+        default:
+          break;
+      }
+    },
+    [keyLocked, dispatch, keyMaps]
+  );
+};
+
+export default useTimerKeyup;
