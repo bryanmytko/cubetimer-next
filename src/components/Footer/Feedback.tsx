6294dee0015e404033623363de2ff7573d90e189
bryan mytko <bryanmytko@gmail.com>
2023-11-19 13:39:14 -0500

    Add contact form

---
+++ src/components/Footer/Feedback.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const Feedback = () => {
+  return (
+    <p className="text-gray-500 text-s text-center pt-3">
+      <Link href="/feedback">Contact Me!</Link>
+    </p>
+  );
+};
+
+export default Feedback;
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,12 @@
-import { CoffeeButton, CopyText } from "./";
+import { CoffeeButton, CopyText, Feedback } from "./";
 
 const Footer = () => {
   return (
     <footer className="lg:h-20 py-4 bg-black sticky top-[100vh] px-2 sm:px-4 flex flex-col lg:flex-row gap-4 lg:gap-8 justify-center">
       <CopyText />
+      <p className="text-gray-500 text-s text-center pt-3">•</p>
+      <Feedback />
+      <p className="text-gray-500 text-s text-center pt-3">•</p>
       <CoffeeButton />
     </footer>
   );
