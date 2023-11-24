fd55ac244f915500cc331981be59b40b02bac41d
bryan mytko <bryanmytko@gmail.com>
2023-11-23 23:02:36 -0500

    Add loading state to confirm modal

--- src/components/Timer/ConfirmModal.tsx
+++ src/components/Timer/ConfirmModal.tsx
@@ -1,8 +1,9 @@
 import { useContext } from "react";
 import { Button } from "@nextui-org/react";
-
 import { Dispatch } from "react";
+
 import { TimerContext, TimerDispatchContext } from "../Timer/TimerContext";
+import { Spinner } from "../../components/Loading";
 import {
   TimerActionKind,
   RecordSolveOptions,
@@ -12,10 +13,10 @@ import {
 
 interface ConfirmProps {
   action: (arg0?: RecordSolveOptions) => Promise<void>;
+  loading: boolean;
 }
 
-const ConfirmModal = (props: ConfirmProps) => {
-  const { action } = props;
+const ConfirmModal = ({ action, loading }: ConfirmProps) => {
   const timer = useContext(TimerContext) as TimerState;
   const dispatch = useContext(TimerDispatchContext) as Dispatch<TimerAction>;
   const { confirmActive } = timer;
@@ -24,27 +25,33 @@ const ConfirmModal = (props: ConfirmProps) => {
 
   return (
     <div className="fixed left-200 inset-28 top-30 z-50 overflow-auto bg-smoke-light flex">
-      <div className="relative p-8 bg-gray-50 max-w-sm m-auto border-2 border-zinc-900 flex-col flex rounded-lg">
+      <div className="relative p-8 bg-gray-50 max-w-sm m-auto border-1 border-zinc-800 flex-col flex rounded-lg">
         <p className="pb-6">Would you like to save this time?</p>
-        <div className="flex flex-col md:flex-row gap-2 md:gap-4">
-          <Button color="success" onClick={() => action()}>
-            Confirm
-          </Button>
-          <Button
-            color="warning"
-            onClick={() => {
-              action({ penalty: 2000 });
-            }}
-          >
-            +2
-          </Button>
-          <Button
-            color="danger"
-            onClick={() => dispatch({ type: TimerActionKind.CANCEL_SOLVE })}
-          >
-            Reject
-          </Button>
-        </div>
+        {loading ? (
+          <div className="flex justify-center">
+            <Spinner />
+          </div>
+        ) : (
+          <div className="flex flex-col md:flex-row gap-2 md:gap-4">
+            <Button color="success" onClick={() => action()}>
+              Confirm
+            </Button>
+            <Button
+              color="warning"
+              onClick={() => {
+                action({ penalty: 2000 });
+              }}
+            >
+              +2
+            </Button>
+            <Button
+              color="danger"
+              onClick={() => dispatch({ type: TimerActionKind.CANCEL_SOLVE })}
+            >
+              Reject
+            </Button>
+          </div>
+        )}
         <span className="absolute top-0 right-0 p-4"></span>
       </div>
     </div>
