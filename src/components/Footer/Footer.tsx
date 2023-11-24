234dc406a944032a7b3785383d4e6cec796ccc4c
bryan mytko <bryanmytko@gmail.com>
2023-11-23 23:20:31 -0500

    Add updates page

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,19 @@
-import { CoffeeButton, CopyText, Feedback } from "./";
+import Link from "next/link";
+
+import { CoffeeButton, CopyText } from "./";
 
 const Footer = () => {
   return (
-    <footer className="lg:h-20 py-4 bg-black sticky top-[100vh] px-2 sm:px-4 flex flex-col lg:flex-row gap-4 lg:gap-8 justify-center">
+    <footer className="lg:h-20 py-4 bg-black sticky top-[100vh] px-2 sm:px-4 flex flex-col lg:flex-row gap-4 lg:gap-4 justify-center">
       <CopyText />
-      <p className="text-gray-500 text-s text-center pt-3">•</p>
-      <Feedback />
-      <p className="text-gray-500 text-s text-center pt-3">•</p>
+      <p className="text-gray-700 text-s text-center pt-3">|</p>
+      <p className="text-gray-500 text-s text-center pt-3">
+        <Link href="/updates">Site Updates</Link>
+      </p>
+      <p className="text-gray-700 text-s text-center pt-3">|</p>
+      <p className="text-gray-500 text-s text-center pt-3">
+        <Link href="/feedback">Contact Me</Link>
+      </p>
       <CoffeeButton />
     </footer>
   );
--- src/components/Footer/index.ts
+++ src/components/Footer/index.ts
@@ -1,4 +1,3 @@
 export { default as Footer } from "./Footer";
 export { default as CoffeeButton } from "./CoffeeButton";
 export { default as CopyText } from "./CopyText";
-export { default as Feedback } from "./Feedback";
