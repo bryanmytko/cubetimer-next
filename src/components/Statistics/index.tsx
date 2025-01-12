6ce848c0f5fe22449530851591b8e1fa9fe797b1
bryan mytko <bryanmytko@gmail.com>
2025-01-12 04:37:33 -0500

    Add classic solve data table

--- src/components/Statistics/index.tsx
+++ src/components/Statistics/index.tsx
@@ -1,4 +1,5 @@
 export { default as ClassicSolves } from "./ClassicSolves";
 export { default as DataTable } from "./DataTable";
+export { default as DataTableClassic } from "./DataTableClassic";
 export { default as Error } from "./Error";
 export { default as Solves } from "./Solves";
