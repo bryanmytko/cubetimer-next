cc0f0d90da39592f3042c7cc0d34db4ee5438c73
bryan mytko <bryanmytko@gmail.com>
2025-01-12 04:32:57 -0500

    Add Classic data table

---
+++ src/components/Statistics/DataTableClassic.tsx
@@ -0,0 +1,23 @@
+const DataTableClassic = ({ children }: any) => {
+  return (
+    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+      <thead className="text-xs text-black uppercase bg-yellow-400">
+        <tr>
+          <th scope="col" className="w-2"></th>
+          <th scope="col" className="px-1 md:px-6 py-3 pr-2">
+            Date
+          </th>
+          <th scope="col" className="pl-2 md:pl-6 pr-2 py-3">
+            Average
+          </th>
+          <th scope="col" className="px-1 md:px-6 py-3 pr-2">
+            Puzzle
+          </th>
+        </tr>
+      </thead>
+      {children}
+    </table>
+  );
+};
+
+export default DataTableClassic;
