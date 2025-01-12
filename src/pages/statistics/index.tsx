1b2c3c2a71d2aa846c468dee9f390811cc11c52f
bryan mytko <bryanmytko@gmail.com>
2025-01-12 17:00:59 -0500

    Add hash navigation of statistic tabs

--- src/pages/statistics/index.tsx
+++ src/pages/statistics/index.tsx
@@ -1,23 +1,45 @@
-import { useState, MouseEvent } from "react";
+import { useEffect, useState, MouseEvent } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import Head from "next/head";
 
 import { ClassicSolves, Solves } from "../../components/Statistics";
 
+enum Tabs {
+  SOLVES = "solves",
+  CLASSIC_SESSIONS = "classic_sessions",
+}
+
 const Statistics = () => {
   const { data: session, status } = useSession();
   const router = useRouter();
   const userId = session?.user.id;
 
-  const [activeTab, setActiveTab] = useState("1");
+  const [activeTab, setActiveTab] = useState<Tabs>(Tabs.SOLVES);
 
   const changeTab = (e: MouseEvent<HTMLElement>) => {
     let target = e.target as HTMLElement;
-    if (target.dataset["tab"]) setActiveTab(target.dataset["tab"]);
+    if (target.dataset["tab"]) setActiveTab(target.dataset["tab"] as Tabs);
   };
 
   if (status === "unauthenticated") router.replace("/auth/signin");
+
+  useEffect(() => {
+    const hash = window.location.hash.slice(1);
+
+    switch (hash) {
+      case Tabs.SOLVES:
+        return setActiveTab(Tabs.SOLVES);
+      case Tabs.CLASSIC_SESSIONS:
+        return setActiveTab(Tabs.CLASSIC_SESSIONS);
+      default:
+        return setActiveTab(Tabs.SOLVES);
+    }
+  }, []);
+
+  useEffect(() => {
+    window.location.hash = activeTab;
+  }, [activeTab]);
 
   return (
     <div className="container statistics-container m-auto px-0 lg:px-0 py-6 md:py-12 w-full text-black">
@@ -29,28 +51,30 @@ const Statistics = () => {
         <ul className="tabsNav flex">
           <li
             className={`cursor-pointer rounded-t-md mr-1 px-4 py-2 ${
-              activeTab === "1" ? "bg-yellow-400" : "bg-gray-500"
+              activeTab === Tabs.SOLVES ? "bg-yellow-400" : "bg-gray-500"
             }`}
-            data-tab="1"
+            data-tab={Tabs.SOLVES}
             onClick={changeTab}
           >
             Solves
           </li>
           <li
             className={`cursor-pointer rounded-t-md px-4 py-2 ${
-              activeTab === "2" ? "bg-yellow-400" : "bg-gray-500"
+              activeTab === Tabs.CLASSIC_SESSIONS
+                ? "bg-yellow-400"
+                : "bg-gray-500"
             }`}
-            data-tab="2"
+            data-tab={Tabs.CLASSIC_SESSIONS}
             onClick={changeTab}
           >
-            Sessions
+            Classic Sessions
           </li>
         </ul>
       </div>
-      <div className={activeTab === "1" ? "" : "hidden"}>
+      <div className={activeTab === Tabs.SOLVES ? "" : "hidden"}>
         <Solves userId={userId} />
       </div>
-      <div className={activeTab === "2" ? "" : "hidden"}>
+      <div className={activeTab === Tabs.CLASSIC_SESSIONS ? "" : "hidden"}>
         <ClassicSolves userId={userId} />
       </div>
     </div>
