b15a7df28e4568499508fc0b3ede67d4c1ef2f31
bryan mytko <bryanmytko@gmail.com>
2025-01-23 22:36:27 -0500

    Add scramble refresh button

--- src/components/Scramble/Scramble.tsx
+++ src/components/Scramble/Scramble.tsx
@@ -1,14 +1,27 @@
-import { useContext } from "react";
-import { TimerContext } from "../Timer/TimerContext";
-import { TimerState } from "../../types/timer";
+import { Dispatch, useContext } from "react";
+import { FaUndoAlt } from "react-icons/fa";
+
+import { TimerContext, TimerDispatchContext } from "../Timer/TimerContext";
+import { TimerAction, TimerActionKind, TimerState } from "../../types/timer";
 
 const Scramble = () => {
   const timer = useContext(TimerContext) as TimerState;
+  const dispatch = useContext(TimerDispatchContext) as Dispatch<TimerAction>;
+
+  const newScramble = () => dispatch({ type: TimerActionKind.INITIALIZE });
 
   return (
-    <p className="text-white text-balance [text-wrap:balance] text-lg md:text-xl lg:text-2xl text-center mt-6 px-4 md:px-0">
-      {timer.scramble}
-    </p>
+    <div className="flex align-middle justify-center gap-2 mt-6">
+      <p className="text-white text-balance text-lg md:text-xl lg:text-2xl text-center px-4 md:px-0">
+        {timer.scramble}
+      </p>
+      <button onClick={newScramble}>
+        <FaUndoAlt
+          size={17}
+          className="text-gray-500 hover:text-yellow-300 mb-1"
+        />
+      </button>
+    </div>
   );
 };
 
